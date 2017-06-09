16dcd05b7f2a8015582293a7a410dfad8980845c
Владислав <vlad-s-d@ya.ru>
2017-06-09 12:38:59 +0300

    Добовляю спиннер. Забил на ротацию

--- app/src/main/java/com/home/nocrail/numerator/About.java
+++ app/src/main/java/com/home/nocrail/numerator/About.java
@@ -1,11 +1,14 @@
 package com.home.nocrail.numerator;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class About extends AppCompatActivity {
 
@@ -13,6 +16,14 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Button git = (Button)findViewById(R.id.github);
+        git.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/NoCrail/Numerator"));
+                startActivity(browserIntent);
+            }
+        });
 
 
     }
